8953807fe63c7b07d8f14dbf451782ef06a55714
WPAI-MrWizard94 <rob@wpaistudio.net>
2026-09-21 03:30:20 -0400

    Remove deprecated explicit mod-bus selector

--- src/main/java/com/mrwizard/generatedferallanterns/client/ClientModEvents.java
+++ src/main/java/com/mrwizard/generatedferallanterns/client/ClientModEvents.java
@@ -6,7 +6,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 
-@EventBusSubscriber(modid = GeneratedFeralLanterns.MOD_ID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = GeneratedFeralLanterns.MOD_ID, value = Dist.CLIENT)
 public final class ClientModEvents {
     private ClientModEvents() {
     }
