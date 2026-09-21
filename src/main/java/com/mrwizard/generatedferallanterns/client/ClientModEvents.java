3f6cb618405d3649269e7773d45e5e6895cb2750
WPAI-MrWizard94 <rob@wpaistudio.net>
2026-09-21 03:24:17 -0400

    Add migration UI and networking

---
+++ src/main/java/com/mrwizard/generatedferallanterns/client/ClientModEvents.java
@@ -0,0 +1,18 @@
+package com.mrwizard.generatedferallanterns.client;
+
+import com.mrwizard.generatedferallanterns.GeneratedFeralLanterns;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
+
+@EventBusSubscriber(modid = GeneratedFeralLanterns.MOD_ID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
+public final class ClientModEvents {
+    private ClientModEvents() {
+    }
+
+    @SubscribeEvent
+    public static void registerScreens(RegisterMenuScreensEvent event) {
+        event.register(GeneratedFeralLanterns.MIGRATION_MENU.get(), MigrationPromptScreen::new);
+    }
+}
